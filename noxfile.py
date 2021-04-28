571ab657f58453e54dc23c4073314b891f3cd405
Chad Hattabaugh <Chad.Hattabaugh@GMail.com>
2021-04-28 12:59:09 -0400

    updates to forward args from nox to pytest

--- noxfile.py
+++ noxfile.py
@@ -2,5 +2,6 @@ import nox
 
 @nox.session(python=["3.8.9", "3.9.4"])
 def tests(session): 
+    args = session.postargs or ["--cov"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", args)
