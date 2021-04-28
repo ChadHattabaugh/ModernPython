b8d3634803a925be14d2f11a3ab1b031f90235a1
Chad Hattabaugh <Chad.Hattabaugh@GMail.com>
2021-04-28 13:01:06 -0400

    fixed issue in noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,6 @@ import nox
 
 @nox.session(python=["3.8.9", "3.9.4"])
 def tests(session): 
-    args = session.postargs or ["--cov"]
+    args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", args)
