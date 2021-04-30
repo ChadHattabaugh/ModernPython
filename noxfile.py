6d77bf72b5595396731a173fda970d6541b8d996
Chad Hattabaugh <Chad.Hattabaugh@GMail.com>
2021-04-30 10:10:44 -0400

    Added type cheekers Mypy and pytype

--- noxfile.py
+++ noxfile.py
@@ -5,20 +5,38 @@ import nox
 python_versions = ["3.8.9", "3.9.4"]
 locations = "src", "tests", "noxfile.py"
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "tests", "mypy", "pytype"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=python_versions)
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e3e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
 
 
 @nox.session(python=python_versions)
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-black",
         "flake8-isort",
@@ -32,7 +50,7 @@ def lint(session):
 @nox.session(python=python_versions)
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -48,5 +66,19 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=python_versions)
+def mypy(session) -> None:
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
+
+
+@nox.session(python="3.8.9")
+def pytype(session):
+    args = session.posargs or ["--disable=import-error", *locations]
+    install_with_constraints(session, "pytype")
+    session.run("pytype", *args)
