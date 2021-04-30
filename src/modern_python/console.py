8bdb965aea7209224dbbb54b25926d1dd078f83b
Chad Hattabaugh <Chad.Hattabaugh@GMail.com>
2021-04-30 11:47:17 -0400

    added typing and data validation

--- src/modern_python/console.py
+++ src/modern_python/console.py
@@ -4,8 +4,6 @@ import click
 
 from . import __version__, wikipedia
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
 
 @click.command()
 @click.option(
@@ -17,12 +15,9 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language):
+def main(language: str) -> None:
     """The Modern python template"""
-    data = wikipedia.random_page(language=language)
+    page = wikipedia.random_page(language=language)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
